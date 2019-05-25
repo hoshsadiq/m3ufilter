7949a1ac20ecca159aa51b3b01428836e2904bd3
Hosh Sadiq <hoshsadiq@users.noreply.github.com>
2019-05-25 23:41:48 +0100

    pipeline fixes

--- writer/m3u.go
+++ writer/m3u.go
@@ -8,6 +8,6 @@ import (
 func writeM3U(w io.Writer, mediaPlaylist *m3u8.MediaPlaylist) {
 	_, err := w.Write(mediaPlaylist.Encode().Bytes())
 	if err != nil {
-		log.Errorf("unable to write new playlist, err = %v", mediaPlaylist, err)
+		log.Errorf("unable to write new playlist, err = %v", err)
 	}
 }
