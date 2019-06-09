12730ad8448010f8fc1301edfafc026644c52dfc
Hosh Sadiq <hoshsadiq@users.noreply.github.com>
2019-06-09 12:19:08 +0100

    final travis build

--- ci/docker-manifest.sh
+++ ci/docker-manifest.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-set -eux
+set -eu
 
 token="$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:$TRAVIS_REPO_SLUG:pull" | jq -r '.token')"
 allTags="$(curl -Ls "https://index.docker.io/v2/$TRAVIS_REPO_SLUG/tags/list" -H "Authorization: Bearer $token" | jq '.tags')"
 
 manifestLists=()
-versions=("${TRAVIS_TAG:-v0.0.0}" "latest")
+versions=("${TRAVIS_TAG}" "latest")
 for version in "${versions[@]}"; do
     tags=($(echo "$allTags" | jq -r --arg version "$version" '.[] | select(. | startswith($version))'))
     manifests=()
