e76b740ea4a6b8bf4adb771a118448b726c89e4e
Hosh Sadiq <hoshsadiq@users.noreply.github.com>
2019-06-09 03:00:56 +0100

    manifest

--- ci/docker-manifest.sh
+++ ci/docker-manifest.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
-docker images
+set -eux
+
+token="$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:$TRAVIS_REPO_SLUG:pull" | jq -r '.token')"
+allTags="$(curl -Ls "https://index.docker.io/v2/$TRAVIS_REPO_SLUG/tags/list" -H "Authorization: Bearer $token" | jq '.tags')"
+
+versions=("${TRAVIS_TAG}" "latest")
+for version in "${versions[@]}"; do
+    tags=($(echo "$allTags" | jq -r --arg version "$version" '.[] | select(. | startswith($version))'))
+    manifests=()
+    for tag in "${tags[@]}"; do
+        arch=${tag#"$version-"}
+        manifests+=("$TRAVIS_REPO_SLUG:$tag")
+#        echo "tag=$tag; version=$version; arch=$arch"
+    done
+    docker manifest create "$TRAVIS_REPO_SLUG:$version" "${manifests[@]}"
+done
