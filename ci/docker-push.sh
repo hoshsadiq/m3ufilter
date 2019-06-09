c0d9122e7b7fa18dcc50fc9ea0fb499e59b57f39
Hosh Sadiq <hoshsadiq@users.noreply.github.com>
2019-06-09 01:54:36 +0100

    try docker push

--- ci/docker-push.sh
+++ ci/docker-push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker tag $TRAVIS_REPO_SLUG:${TRAVIS_TAG}-$ARCH $TRAVIS_REPO_SLUG:latest-$ARCH
+docker tag $TRAVIS_REPO_SLUG:${TRAVIS_TAG:-v0.0.0}-$ARCH $TRAVIS_REPO_SLUG:latest-$ARCH
 
-docker push $TRAVIS_REPO_SLUG:${TRAVIS_TAG}-$ARCH
+docker push $TRAVIS_REPO_SLUG:${TRAVIS_TAG:-v0.0.0}-$ARCH
 docker push $TRAVIS_REPO_SLUG:latest-$ARCH
